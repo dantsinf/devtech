64cdcbe6d1e3be28968c84e75608c8031d9788c4
Dante Rosa Ulguim <dante_ulguim@sicredi.com.br>
2021-11-21 19:26:07 -0300

    aula 4, colecoes

---
+++ java/ex04/ExColecoes/src/ExemploMaps.java
@@ -0,0 +1,21 @@
+package ExColecoes.src;
+
+import java.util.HashMap;
+
+public class ExemploMaps {
+    public void demo1(){
+       /* HashMap<String, ContaCorrente> tab = new HashMap<>();
+
+        tab.put("Felipe", new ContaCorrente(123456));
+        tab.put("Carol", new ContaCorrente(345784));
+        tab.put("Eduardo", new ContaCorrente(968457));
+
+        System.out.printf("Dados do Felipe: " + tab.get("Felipe").getNroConta());
+        System.out.printf("Dados do Carol: " + tab.get("Carol").getNroConta());
+        System.out.printf("Dados do Felipe: " + tab.get("Felipe").getNroConta());
+
+        System.out.printf("Dados do Edi: " + tab.get("Edi").getNroConta());
+        */
+
+    }
+}
