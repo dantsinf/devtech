0c376ea5292f08ad24e5e244533938304a116bb6
Dante Rosa Ulguim <dante_ulguim@sicredi.com.br>
2021-11-23 12:02:34 -0300

    exercicio

---
+++ java/aula05/src/emAula/src/Produto.java
@@ -0,0 +1,52 @@
+package emAula.src;
+
+public class Produto {
+        private String cod;
+        private String descricao;
+        private int qtde;
+
+        public Produto(String c, String d, int q){
+            cod= c;
+            descricao = d;
+            qtde = q;
+        }
+
+        public String getCod(){
+            return cod;
+        }
+
+        public String getDescricao(){
+            return descricao;
+        }
+
+        public int getQtde(){
+            return qtde;
+        }
+
+        public void  colocaEstoque(int n){
+            if (n >= 0)
+                qtde += n;
+        }
+
+        public int retiraEstoque(int n){
+            int res = n;
+
+            if (n >= 0)
+                if (n < qtde)
+                    qtde -= n;
+                else {
+                    res = qtde;
+                    qtde = 0;
+                }
+
+            return res;
+
+        }
+
+
+        public String toString(){
+            return "Código: "+getCod()+
+                    "\n\tDescrição: "+getDescricao()+
+                    "\n\tQualidade: "+qtde;
+        }
+}
---
+++ java/aula05/src/emAula/src/ProdutoPerecivel.java
@@ -0,0 +1,43 @@
+package emAula.src;
+
+import java.time.*;
+
+public class ProdutoPerecivel extends Produto{
+    private LocalDate dataValidade;
+
+    public ProdutoPerecivel(String c, String d, int q, LocalDate dt){
+        super(d, d, q); //construtor super, sem ele o java tenta chamar ele novamente e fica super();
+        dataValidade = dt;
+    }
+
+    public int retiraEstoque(int n, LocalDate dtCOrrente) {
+        int res = n;
+        Period difDate = Period.between(dtCOrrente, dataValidade);
+
+        if(difDate.getMonths()>2 || (difDate.getMonths() == 2 && difDate.getDays() > 0))
+            res = 0;
+            else res = super.retiraEstoque(n);
+            return res;
+    }
+
+    /*Não necessário sobrescrever a original. Se não, temos ocmportamento inconsistente.*/
+
+    public int retiraEstoque(int n){
+        return retiraEstoque(n, LocalDate.now());
+    }
+
+    public void  colocaEstoque(int n, LocalDate dtCorrente){
+        if(dtCorrente.equals(dataValidade))
+            super.colocaEstoque(n);
+    }
+
+    /*Não necessário sobrescrever a original. Se não, temos ocmportamento inconsistente.*/
+
+    public void colocaEstoque(int n){
+        colocaEstoque((n, LocalDate.now());
+    }
+
+
+
+
+}
