e87372f6d529d811b0ec07c0acf9aecd94803ea8
Dante Rosa Ulguim <dante_ulguim@sicredi.com.br>
2021-11-29 11:30:56 -0300

    classe Person e md README

---
+++ java/aula07/src/emAula/src/Person.java
@@ -0,0 +1,28 @@
+package emAula.src;
+
+public class Person {
+    public enum Sex{ MALE,FEMALE }
+    private String nome;
+    private int idade;
+    private Sex sexo;
+
+    public Person(String nome, int idade, Sex sexo){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public String name() {return nome;}
+
+    public int age() {return idade;}
+
+    public Sex gender() {return sexo;}
+
+    @Override
+    public String toString() {
+        return nome + "-" +
+                idade + "-" +
+                sexo;
+    }
+
+}
