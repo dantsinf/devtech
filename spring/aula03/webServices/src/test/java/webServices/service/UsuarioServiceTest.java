49a9d88384c0b01499a5cfa74a01a23c3c4fbb31
Dante <layscontato@gmail.com>
2021-12-10 10:33:36 -0300

    classe UsuarioServiceTest finalizado

--- spring/aula03/webServices/src/test/java/webServices/service/UsuarioServiceTest.java
+++ spring/aula03/webServices/src/test/java/webServices/service/UsuarioServiceTest.java
@@ -9,28 +9,42 @@ public class UsuarioServiceTest {
     private Usuario usuario;
 
     @BeforeEach
-    public void beforeAll(){
+    public void beforeAll() {
         usuarioService = new UsuarioService();
         usuarioService.iniciar();
     }
 
     @Test
-    void cadastraUsuario(){
-        usuario = new Usuario("Claudio","001", TipoUsuario.LOCADOR);
+    void cadastraUsuario() {
+        usuario = new Usuario("Claudio", "001", TipoUsuario.LOCADOR);
         String usuarioSalvo = usuarioService.cadastraUsuario(usuario);
         Assertion.assertEquals(usuarioSalvo, "Usuário cadastrado com sucesso!");
     }
 
     @Test
-    void usuarioJaExiste(){
-        usuario = new Usuario("Claudio","001", TipoUsuario.LOCADOR);
+    void usuarioJaExiste() {
+        usuario = new Usuario("Claudio", "001", TipoUsuario.LOCADOR);
         String usuarioSalvo = usuarioService.cadastraUsuario(usuario);
         Assertion.assertEquals(usuarioSalvo, "Usuário cadastrado com sucesso!");
     }
 
+    @Test
+    void buscaUsuario() {
+        usuario usuarioSalvo = usuarioService.buscaUsuario("Lia")
+        Assertion.assertEquals(usuarioSalvo.getUser(), "Lia");
+    }
 
+    @Test
+    void delete() {
+        String resposta = usuarioService.delete("Lia");
+        Assertion.assertEquals(resposta, "Remova com sucesso!");
+    }
 
+    @Test
+    void valida() {
+        Assertion.assertEquals(usuarioService.valida("Lia", "123"), "Locador Logado");
+    }
 
-
+}
 
 }
