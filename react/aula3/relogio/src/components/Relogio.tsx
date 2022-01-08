034403eb1086fef61d7109f4207209c279f36f64
Dante <layscontato@gmail.com>
2022-01-08 20:15:13 -0300

    renomeando variaveis e corrigindo erro

--- react/aula3/relogio/src/components/Relogio.tsx
+++ react/aula3/relogio/src/components/Relogio.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Relogio.css'
 
 const Relogio = () => {
-  const hora = new Date().toLocaleTimeString();
+  let tempo = new Date().toLocaleTimeString();
+  const [otempo, setOtempo] = useState(tempo);
+
+  const AtualizaHora = () => {
+    tempo = new Date().toLocaleTimeString();
+    setOtempo(tempo);  
+  }
+
+  setInterval(AtualizaHora, 1000);
 
   return (
-    <div className='principal' >
-      <h1>{hora}</h1>
-    </div>
+    <>
+      <div className='principal' >
+        <h1>{otempo}</h1>
+          <div>
+            <button>Pega hor</button>
+          </div>
+      </div>
+    </>
   )
 }
 
